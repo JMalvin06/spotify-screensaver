465f60f124aa29d5177aee17ec66ff7cffcf83e0
John Malvin <john.w.malvin@gmail.com>
2024-11-18 21:45:53 -0600

    Display current track and sign in logic

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
-use iced::{alignment::Vertical::Top, widget::{container, text, Container}, Alignment::Center, Length, Task};
+use core::fmt;
+
+use iced::{widget::{button, container, row, text, text_input, Container}, Alignment::Center, Length, Task};
 use iced::widget::column;
 mod spotify;
 
 #[derive(Default)]
 struct LoginMenu{
-    selection: String
+    selection: String,
+    token: String,
+    content: Status,
+    input: String,
+    current_track: String
 }
 
 #[derive(Clone, Debug)]
 enum Message{
-    SelectedUser(String)
+    SelectedUser(String),
+    NextPage,
+    SignIn,
+    InputValue(String),
+    ToSelection,
+    RefreshTrack
+}
+
+enum Status{
+    UserSelect,
+    SignIn,
+    CurrentTrack
+}
+impl Default for Status {
+    fn default() -> Self { Status::UserSelect }
 }
 
 
@@ -21,45 +41,114 @@ impl LoginMenu{
 
     fn new() -> (LoginMenu, Task<Message>){
         (Self{
-            selection: String::from("Please select a user.."),
-            //user_list: todo!(),
+            selection: String::from("Select a user.."),
+            token: String::new(),
+            content: Status::UserSelect,
+            input: String::default(),
+            current_track: String::from("None")
         }, Task::none())
     }
 
     fn view(&self) -> Container<'_, Message>{
-        //let mut state: State<String> = State::new(vec![String::from("a"), String::from("b")]);
-        let list  = iced::widget::pick_list(spotify::get_user_list(), Some(self.selection.clone()), Message::SelectedUser);
-        
-        container(
-            column![
-            text("User Login")
-            .size(50),
-            list
-            .placeholder("Select a user..")
-            ].align_x(Center)
-        )
-        .height(Length::Fill)
-        .width(Length::Fill)
-        .align_x(Center)
-        .align_y(Top)
-        .padding(100)
-        //.align_y(Center)
+        match self.content {
+            Status::UserSelect => {
+                let list  = iced::widget::pick_list(spotify::get_user_list(), Some(self.selection.clone()), Message::SelectedUser);
+                container(
+                    column![
+                    text("User Login")
+                    .size(50),
+                    list
+                    .placeholder("Select a user.."),
+                    row![
+                        button("Sign In").on_press(Message::SignIn),
+                        button("Next").on_press(Message::NextPage)
+                    ].spacing(50)
+                    ]
+                    .align_x(Center)
+                    .spacing(40)
+                )
+                .height(Length::Fill)
+                .width(Length::Fill)
+                .align_x(Center)
+                .align_y(Center)
+                .padding(100)
+                //.align_y(Center)
+            } 
+            Status::SignIn => {
+                container(
+                    column![
+                        text_input("Spotify Username", &self.input)
+                        .width(250)
+                        .on_input(|value| Message::InputValue(value))
+                        .on_submit(Message::ToSelection),
+                        button("Submit")
+                        .on_press(Message::ToSelection)
+                    ].align_x(Center)
+                )
+                .height(Length::Fill)
+                .width(Length::Fill)
+                .align_x(Center)
+                .align_y(Center)
+                .padding(300)
+            }
+            Status::CurrentTrack => {
+                container(
+                    column![
+                        text("Current Track")
+                        .size(20),
+                        text(self.current_track.clone())
+                        .size(25),
+                        button("Refresh")
+                        .on_press(Message::RefreshTrack)
+                    ]
+                    .spacing(10)
+                    .align_x(Center)
+                )
+                .height(Length::Fill)
+                .width(Length::Fill)
+                .align_x(Center)
+                .align_y(Center)
+                .padding(300)
+            }
+        }
     }
+    
 
     fn update(&mut self, message: Message){
         match message{
             Message::SelectedUser(value) => {
-                self.selection = value
+                self.selection = value;
+            }
+            Message::NextPage => {
+                println!("Task Start");
+                self.token = spotify::generate_token(self.selection.clone());
+                let track = spotify::get_current_track(&self.token);
+                self.current_track = track.name;
+                self.content = Status::CurrentTrack;
+            }
+            Message::SignIn => {
+                self.content = Status::SignIn
+            }
+            Message::InputValue(value) => self.input = value,
+            Message::ToSelection => {
+                //println!("User: {}", self.input);
+                let user = self.input.clone();
+                let code = spotify::retrieve_code();
+                spotify::generate_refresh(user, code);
+
+                self.content = Status::UserSelect
+            }
+            Message::RefreshTrack => {
+                self.token = spotify::generate_token(self.selection.clone());
+                let track = spotify::get_current_track(&self.token);
+                self.current_track = track.name;
             }
         }
     }
 }
 
 
-
-
 fn main() -> iced::Result{
     let app = iced::application(LoginMenu::title, LoginMenu::update, LoginMenu::view);
     app.run_with(LoginMenu::new)
-    //iced::run(LoginMenu::title, |arg0: &mut LoginMenu, message| LoginMenu::update(&*arg0, message) ,LoginMenu::view)
 }
--- src/spotify.rs
+++ src/spotify.rs
@@ -2,8 +2,9 @@ use reqwest::{self, header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE}};
 use serde::{Serialize, Deserialize};
 use url_search_params;
 use open;
+use tokio;
 
-use std::{io::{self, stdin, stdout, Write}, process::Command};
+use std::process::Command;
 use std::collections::HashMap;
 use std::fs;
 
@@ -31,8 +32,8 @@ struct Player{
 
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Track{
-    name: String,
+pub(crate) struct Track{
+    pub name: String,
     artists: Vec<Artist>
 }
 
@@ -55,7 +56,8 @@ struct User {
 }
 
 
-async fn generate_refresh(username: &String, code: String) -> String{
+#[tokio::main]
+pub (crate) async fn generate_refresh(username: String, code: String) -> String{
     println!("Generating Token...");
     let auth_url = "https://accounts.spotify.com/api/token"; 
 
@@ -86,8 +88,13 @@ async fn generate_refresh(username: &String, code: String) -> String{
                     // TODO: Handle json errors less repetitively
                     let file = match fs::read_to_string("users.json"){ 
                         Ok(f) => {
+                            // Cleans whitespace from file (trim does not work for some reason)
+                            let cleaned_file: String = f.chars()
+                            .filter(|c| !c.is_whitespace())
+                            .collect();
+                            
                             // Check if users.json does not follow necessary format, and correct
-                            if f.is_empty() || (!f.trim().contains("{\"users\": [") && !f.trim().contains("]}")){
+                            if f.is_empty() || (!cleaned_file.contains("{\"users\":[") && !cleaned_file.contains("]}")){
                                 fs::write("users.json", "{\"users\": []}").expect("Failed to write");
                                 fs::read_to_string("users.json").expect("Terrible horrible error")
                             } else{
@@ -100,7 +107,7 @@ async fn generate_refresh(username: &String, code: String) -> String{
                             fs::read_to_string("users.json").expect("Terrible horrible error")
                         }
                     };
-                    let mut file: Users = serde_json::from_str(file.as_str()).expect("I will kill you");
+                    let mut file: Users = serde_json::from_str(file.as_str()).expect("Time to cry");
                     let refresh_token = parsed.refresh_token;
                     let mut user_exists = -1;
 
@@ -114,6 +121,7 @@ async fn generate_refresh(username: &String, code: String) -> String{
                     
                     // Assign user with refresh token in users.json
                     if user_exists == -1 { // if user does NOT exist, add new json entry
+                        println!("Old json: {}", serde_json::to_string_pretty(&file).expect("Oopsie"));
                         let new_user: User = User{
                             name: String::from(username.trim()),
                             refresh: String::from(refresh_token)
@@ -123,6 +131,7 @@ async fn generate_refresh(username: &String, code: String) -> String{
                         let new_user = file.users.get_mut(user_exists as usize).unwrap();
                         new_user.refresh = String::from(refresh_token);
                     }
+                    println!("New json: {}", serde_json::to_string_pretty(&file).expect("Oopsie"));
                     fs::write("users.json", serde_json::to_string_pretty(&file).expect("Could not convert")).expect("Could not writes");
                     parsed.access_token // Return recieved access token
                 }
@@ -133,7 +142,9 @@ async fn generate_refresh(username: &String, code: String) -> String{
     }
 }
 
-async fn generate_token(user: &String) -> String{
+#[tokio::main]
+pub (crate) async fn generate_token(user: String) -> String{
+    println!("Generating token!");
     // Convert users.json to struct format
     let file: Users = serde_json::from_str(
         fs::read_to_string("users.json")
@@ -178,7 +189,7 @@ async fn generate_token(user: &String) -> String{
     }
 }
 
-fn retrieve_code() -> String {
+pub(crate) fn retrieve_code() -> String {
     let scope = "user-read-playback-state";
     // Parameters for body of URL link
     let params = HashMap::from([
@@ -189,6 +200,7 @@ fn retrieve_code() -> String {
     ]
     );
 
+    //File::create("code.txt").expect("Could not create file");
     // Open node.js server to listen for authorization code at URI
     Command::new("node")
     .arg("server.js")
@@ -199,13 +211,17 @@ fn retrieve_code() -> String {
     println!("Opening {} on default browser", url_out);
     open::that(url_out).expect("Not a valid URL"); // Automatically open browser at link
 
-    let mut response = String::new();
+    //let mut response = String::new();
     // TODO: Automatically proceed with code once code has been recieved
-    while response.to_lowercase().trim() != "done"{
-        println!("Please enter \"done\" when you have sumbitted openned the link and accepted permissions");
-        response = String::new();
-        stdin().read_line(&mut response).expect("Did not expect that string");
+    loop {
+        if fs::exists("code.txt").expect("File error"){
+            if !fs::read("code.txt").expect("Could not open").is_empty(){
+                println!("Found code");
+                break;
+            }
+        }
     }
+    //while fs::read("code.txt").expect("Could not open").is_empty(){}
     
     // Close the javascript server
     // TODO: Close specific "server.js" server
@@ -216,10 +232,12 @@ fn retrieve_code() -> String {
 
     let code  = fs::read_to_string("code.txt").expect("Could not read file"); 
     fs::remove_file("code.txt").expect("Failed to delete file"); // Remove file with code for security purposes
+    println!("Code: {}", code);
     code // Return authorization code
 }
 
-async fn get_current_track(auth_token: &str) -> Track{
+#[tokio::main]
+pub (crate) async fn get_current_track(auth_token: &str) -> Track{
     let url = format!("https://api.spotify.com/v1/me/player");
 
     let client = reqwest::Client::new(); // Client to handle API request
@@ -258,87 +276,4 @@ pub(crate) fn get_user_list() -> Vec<String>{
         .as_str()
     ).expect("Could not convert to json");
     file.users.iter().map(|u| u.name.clone()).collect() // Parse json file and generate a list of usernames
-}
-#[tokio::main]
-async fn main() {
-    let mut is_new = String::new(); // Stores whether the current user is a new login
-    let mut username = String::new(); // Stores the username value
-    
-    println!("Is this your first time logging in? (y/n)");
-    let _ = stdout().flush();
-    io::stdin().read_line(&mut is_new).expect("Did not expect that string"); // Store response
-
-    let is_new = match is_new.chars().nth(0){ // Convert to boolean value based on response
-        Some(response) => {
-        if response == 'y' {
-            true
-        } else if  response == 'n' {
-            false
-        } else {
-            panic!("Invalid response") // Neither y or n input
-        }
-    },
-        None => panic!("String is empty")
-    };
-
-    let auth_token: String;
-    if is_new { // For new user logins
-        println!("Please input your spotify username: ");
-        let _ = stdout().flush();
-        io::stdin().read_line(&mut username).expect("Did not expect that string"); // Store to username variable
-        username = String::from(username.trim()); // Trim to eliminate unwanted spaces or return keys
-        let code: String = retrieve_code(); // Retrieve authorization code needed for generating the first token
-        auth_token = generate_refresh(&username ,code).await; // Generate refresh token, and store in users.json
-    } else { // For users with existing user record
-        let file: Users = serde_json::from_str(
-            fs::read_to_string("users.json")
-            .expect("Error opening file")
-            .as_str()
-        ).expect("Could not convert to json");
-        let user_list: Vec<String> = file.users.iter().map(|u| u.name.clone()).collect(); // Parse json file and generate a list of usernames
-        
-        // Print simple list for user selection
-        println!("Users logged in: ");
-        for i in 0..user_list.len(){
-            println!("{}. {}", i+1, user_list.get(i).expect("Not allowed"))
-        }
-
-        let mut response = String::new(); // Store user response to list selection
-        let _ = stdout().flush();
-        io::stdin().read_line(&mut response).expect("Did not expect that string");
-        response = response.trim().to_string();
-
-        let response = match response.parse::<i32>(){
-            Ok(n) => {
-                if n > 0 {
-                    n
-                } else {
-                    panic!("Invalid input, please enter integer value more than 0")
-                }
-            },
-            Err(_) if user_list.contains(&response) => {
-                // User responds with username
-                username = response;
-                -1
-            },
-            Err(_) => panic!("Not a valid input")
-        };
-
-        // If response was a number
-        if response > 0 {
-            username = user_list.get((response-1) as usize).unwrap().clone();
-        }
-        auth_token = generate_token(&username).await; // Generate token from refresh
-    }
-
-    let current_track = get_current_track(auth_token.as_str()).await; // Request current track user is playing
-
-    let track_name = current_track.name;
-    // Create String out of list of artists
-    let artists: String = current_track.artists
-    .iter().map(|a| a.name.clone())
-    .collect::<Vec<String>>()
-    .join(", ");
-
-    println!("Current track is: {} by {}", track_name.trim(), artists);
 }
